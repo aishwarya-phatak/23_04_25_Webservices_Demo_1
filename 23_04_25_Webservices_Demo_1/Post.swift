c4b9563d417975b6576c07b77482fd0e5a34b6b1
aishwarya <ashp17star@gmail.com>
2025-06-26 15:57:48 +0530

    Initial Commit

---
+++ 23_04_25_Webservices_Demo_1/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  23_04_25_Webservices_Demo_1
+//
+//  Created by Vishal Jagtap on 26/06/25.
+//
+
+//Model class for Post
+
+struct Post{
+    var userId : Int
+    var id : Int
+    var title : String
+    var body : String
+}
---
+++ 23_04_25_Webservices_Demo_1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  23_04_25_Webservices_Demo_1
+//
+//  Created by Vishal Jagtap on 26/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var url : URL?
+    var urlRequest : URLRequest?
+    var urlSession : URLSession?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        jsonSerialization()
+    }
+    
+    func jsonSerialization(){
+        url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        urlRequest = URLRequest(url: url!)
+        urlSession = URLSession(configuration: .default)
+        
+        let dataTask = urlSession?.dataTask(with: urlRequest!) { data, urlResponse, error in
+            
+            print(data!)
+            print(urlResponse!)
+            print(error)
+            
+            if((urlResponse as! HTTPURLResponse).statusCode == 200){
+                let jsonPostResponse = try! JSONSerialization.jsonObject(with: data!) as! [[String : Any]]
+                
+                print(jsonPostResponse)
+            }
+        }
+        dataTask?.resume()
+    }
+}
