1ea2628a2a9abfcd65e8ed0132457c0f38f21970
aishwarya <ashp17star@gmail.com>
2025-06-28 10:26:09 +0530

    Webservices With TableView

---
+++ 23_04_25_Webservices_Demo_1/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  23_04_25_Webservices_Demo_1
+//
+//  Created by Vishal Jagtap on 28/06/25.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
